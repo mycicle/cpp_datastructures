fd42c423a60c0cda820d1add8542b0fe5f49b6a0
Mycicle <mjm.digregorio@gmail.com>
2020-11-10 14:58:16 -0500

    solar_system: mostly there, bodies not updating
    Signed-off-by: Mycicle <mjm.digregorio@gmail.com>

--- solar_system/solar_system.cc
+++ solar_system/solar_system.cc
@@ -14,14 +14,53 @@ class Vec3d {
 	public: 
 		double x, y, z;
 		Vec3d(): x(0), y(0), z(0) {}
-		Vec3d(double x, double y, double z): x(x), y(y), z(z){}
+		Vec3d(const double x, const double y, const double z): x(x), y(y), z(z){}
 		Vec3d(const Vec3d& orig): x(orig.x), y(orig.y), z(orig.z) {}
 		Vec3d(Vec3d&& orig): x(orig.x), y(orig.y), z(orig.z) {}
 
-		Vec3d& operator =(const Vec3d& orig) {
+		Vec3d operator =(const Vec3d& orig) {
 			x = orig.x;
 			y = orig.y;
 			z = orig.z;
+			
+			return *this;
+		}
+
+		Vec3d operator /(const double scalar) const {
+			Vec3d a(x/scalar, y/scalar, z/scalar);
+			return a;
+		}
+		
+		Vec3d operator +(const Vec3d& vec) const {
+			Vec3d a(vec.x+x, vec.y+y, vec.z+z);
+			return a;
+		}
+
+		Vec3d operator -(const Vec3d& vec) const {
+			return (*this + (-vec));
+		}
+
+		Vec3d operator -() const {
+			return Vec3d(-x, -y, -z);
+		}
+
+		Vec3d operator *(const Vec3d& vec) const {
+			Vec3d a(x*vec.x, y*vec.y, z*vec.z);
+			return a;
+		}
+
+		Vec3d operator *(const double scalar) const {
+			Vec3d a(x*scalar, y*scalar, z*scalar);
+			return a;
+		}
+
+		double mag() const {
+			return sqrt(x*x + y*y + z*z);
+		}
+
+		Vec3d unitVector() const {
+			Vec3d unit = (*this) * (1/(*this).mag());
+			return unit;
 		}
 
 		friend double dot(const Vec3d& left, const Vec3d& right) {
@@ -39,6 +78,7 @@ class Vec3d {
 
 class Body {
 	private:
+		const double G = 6.67430e-11;
 		Vec3d pos, vel, acc;
 		double mass, radius, orb; //orb = orbital distance
 		string name;
@@ -51,13 +91,46 @@ class Body {
 		Body(const Body& o): pos(o.pos), vel(o.vel), acc(o.acc), mass(o.mass), radius(o.radius), orb(o.orb), name(o.name) {}
 		Body(Body&& o): pos(o.pos), vel(o.vel), acc(o.acc), mass(o.mass), radius(o.radius), orb(o.orb), name(o.name) {}
 
-		Body& operator =(const Body& o) {
+		Body operator =(const Body& o) {
 			pos = o.pos; vel = o.vel; acc = o.acc; mass = o.mass; 
 			radius = o.radius; name = o.name;
+			return *this;
 		}
 
-		void update(const Vec3d& p, const Vec3d& v, const Vec3d& a) {
-			pos = p; vel = v; acc = a;
+		Vec3d calcGravForce(Body& body) {
+			// gravitational force equation
+			double force = (G * mass * body.mass) / ((body.pos - pos).mag() * (body.pos - pos).mag());
+			Vec3d forceVector = (body.pos-pos).unitVector() * force;
+			return forceVector;
+		}
+
+		void update(vector<Body>& bodies, const double dt) {
+			Vec3d netForce;
+			for (auto body : bodies) {
+				if(body.name != name) {
+					netForce = netForce + calcGravForce(body);
+				}
+			}
+			if (name == "Earth") {
+				cout << acc << endl;
+			}
+			Vec3d new_acc = acc + (netForce/mass); // af = ai + dF/m <-> F = ma
+			if (name == "Earth") {
+				cout << "new" << new_acc << endl;
+			}
+			Vec3d new_pos = pos + vel*dt + acc*0.5*pow(dt, 2); // xf = xi + vi*t + 1/2 * a * t^2
+			Vec3d new_vel = vel + acc*dt; //vf = vi + a*dt
+
+			this->pos = new_pos;
+			this->vel = new_vel;
+			this->acc = new_acc;
+			if (name == "Earth") {
+				cout << "newnew" << acc << endl;
+			}
+		}
+
+		Vec3d getPos() const {
+			return pos;
 		}
 
 		friend ostream& operator <<(ostream& s, const Body& b) {
@@ -82,53 +155,57 @@ class Body {
 		}
 };
 
-// class Body {
-// 	private:
-// 		string name;
-// 		double mass, radius;
-// 		Vec3d position;
-// 		int random = distribution(gen);
-// 	public:
-// 		Body() {}
+class SolarSystem {
+	private:
+		vector<Body> bodies;
+	public:
+		SolarSystem(string filepath) {
+			stringstream ss;
+			string line;
+			string name, orbits, mass, diam, rest;
+			ifstream f(filepath);
+			int counter = 0;
+			while(getline(f, line)) {
+				if (line.substr(0, 4) == "Name")
+					continue;
+				Body b;
+				ss << line;
+				ss >> b;
+				bodies.push_back(b);
+				counter ++;
+				ss.clear();
+			}
+		}
 
-// 		friend ostream& operator <<(ostream& s, Body &b) {
-// 			return s << "Name: " << b.name << " Mass: " << b.mass << " Radius: " << b.radius << " Position: " << b.position;
-// 		}
+		SolarSystem(vector<Body> bodies): bodies(bodies) {}
+		void timeStep(double dt) {
+			for (auto body : bodies) {
+				body.update(bodies, dt);
+			}
+		}
 
-// 		friend void operator >>(istream &out, Body &b) {
-// 				string i;
-// 				vector<string> attribs;
-// 				while (out >> i) {
-// 					attribs.push_back(i);
-// 				}
-// 				if (attribs.size() != 10) {
-// 					throw length_error("Invalid input length. Expected 10 columns");
-// 				}
-// 				b.name = attribs.at(0);
-// 				b.mass = stod(attribs.at(2));
-// 				b.radius = stod(attribs.at(3))/2;
-// 				double rad = stod(attribs.at(4)) + stod(attribs.at(5)) / 2;
-// 				b.position = Vec3d(rad*cos(b.random), rad*sin(b.random), 0);
-// 		}
-// };
+		friend ostream& operator <<(ostream& s, const SolarSystem& ss) {
+			for (auto body : ss.bodies) {
+				s << body.getPos() << endl;
+			}
+			// s << endl;
+			return s;
+		}
+};
 
 int main() {
-	stringstream ss;
-	string line;
-	string name, orbits, mass, diam, rest;
-	vector<Body> bodies;
-	ifstream f("solarsystem.dat");
-
-	int counter = 0;
-	while(getline(f, line)) {
-		if (line.substr(0, 4) == "Name")
-			continue;
-		Body b;
-		ss << line;
-		ss >> b;
-		cout << b << endl;
-		bodies.push_back(b);
-		counter ++;
-		ss.clear();
-	}
+	SolarSystem s("solarsystem.dat"); // this must not change
+    // each body should have a random position in a circular or elliptical orbit around the sun
+    // each  body should start with an appropriate velocity
+    //each body should have velocity = https://en.wikipedia.org/wiki/Circular_orbit v= sqrt(GM/r)
+    double earthYear = 365.2425 *  24  * 60 * 60;
+    const int numTimeSteps = 1000;
+    double dt = earthYear / numTimeSteps;
+	// cout << s;
+    for (int i = 0; i < numTimeSteps; i++)
+        s.timeStep(dt);
+    // cout << s; // print out the state of the solar system (where each object is, location only)
+	
+	return 0;
+    // if you do it right, earth should be in roughly the same place...
 }
