28014c8c36d7958e3b36111e9637f6b4f295b875
Mycicle <mjm.digregorio@gmail.com>
2020-11-12 12:30:20 -0500

    dsa: revised comments on question 5
    Signed-off-by: Mycicle <mjm.digregorio@gmail.com>

--- datastructures_and_algos/project_euler_problems/problem5.cc
+++ datastructures_and_algos/project_euler_problems/problem5.cc
@@ -10,14 +10,8 @@ int main() {
     long int lcm;
     long int iterations = 0;
     bool multiple = true;
-    // 380 is the lcm of 20 and 19
-    // we will not have to consider the factors of 20 or 19
     for (long int i = 2520; i <= 1e9; i += 2520) {
-        // we dont need to consider the following numbers
-        /*
-            2, 5, 4, 8, 10, 20, 19, 16
-        */
-       
+
        for (int j = 11; j <=19; j++) {
            iterations++;
            if (i%j != 0) {
